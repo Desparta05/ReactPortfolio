50495fe1c0fc19cd3a5e0c764d567d00194421d5
desparta05 <desparta05@gmail.com>
2021-05-19 19:31:11 -0500

    Created Footer

---
+++ client/src/componets/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <div className="footer">
+            <span>
+                Powered by the powerful React Library!
+                For inquiries, contact me at <a href="mailto:Desparta05@gmail.com"> Desparta05@gmail.com</a>.
+        </span>
+        </div>
+    )
+}
+
+export default Footer;
