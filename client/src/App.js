901687f69627e7439f0ad745568ced5b3479b022
desparta05 <desparta05@gmail.com>
2021-05-19 19:29:12 -0500

    Added routes to the pages

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './layouts/Main';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import './styles/App.css'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Main />}>
+          <Route path="/" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+
+export default App;
