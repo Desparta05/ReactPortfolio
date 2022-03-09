bcc9ed8903978d9da9cd6dc62db214c063d2029d
Desparta05 <Desparta05@gmail.com>
2022-03-08 23:31:01 -0600

    Added Data.js for array of projects

---
+++ client/src/data.js
@@ -0,0 +1,38 @@
+export const portfolios = [
+    {
+        id:1,
+        img: "https://lh3.googleusercontent.com/kjRd9kFno8aoLhnBxj70pb5nD96UDMLgnQzHddg9t2-wd8ePIFMJ8lpspZJtP6q8mJYgWJ6H7jYL_7BD5dj8ms5t3FR4v08JfattA4wUjdiN5zkFth7g2s-QWSoXn99vMyqq4HvyhkwgwVNMtkZQyK1IHh4LmPA1dPXtz3y86acwC9EXd_3SAxNnaswBCoKnBsQ_YXiLpqg4SyK4DS-hRJWkqv9KqVEplmkc-POEGWQrKKATaKzG-PeBZNDodME_rQ2Ta9lV2t2Czn26fNUflGzZ-47mJHRjK4DdsQ_-tqdPfZ3NiHZII7DspfHkL1sjC0Ekxv5EJ2UzrnH_wliP2FcDjiQ1phIp1nlfpeZynEhLws-Oz5UEM4OFRMLBq0czjao592u61LoMtybj4Q9KqKm-QCRcRG5XXMAUx4egz3UfuBcng3nVfuPUcYOcGBWaSXkM4Ls8UeGGM-Bn2EJ9dFU2NQYjQjc1bSXSBFj7LqkAVUTTCQYL2ByhyzlbQYxbO1MpQImyL5Hhdep2TRBfQe9tWCdhWPAgG91PDxvYchedfub_qACxM0uiXW0PtFK32NJr-ZqPmFIHVeVYefEYrKpmF9pIXsNt923r-kNNFWUyWghoFv0pHtzBeSm8ZCe00WDGWTTKLWWHnNTVyt-ayEv8salboFMWTD_vPpX6GcpruHw--OnB-mY_8OGCx2IIth3QPl9AJPnPtNLDoq2nM_Tz=w351-h929-no?authuser=0",
+        link: "https://afternoon-dusk-39802.herokuapp.com/",
+        text: "DC Database"
+    },
+    {
+        id:2,
+        img: "https://lh3.googleusercontent.com/ioLKsxfJmnHU6Cq9SJu9MpObCfT2HNOXNwV4rGvXpoe7-LfNd4CFyUQtEhQt0JHoF1FINUljRT_smUQzU1qpMgIm8A93lh2jteU04-UPkaZjx9euJAwlbIO-Ps9dbpnGRYjBR7o5N_6-ZWI7n4_ruz12ge9uqZsBWMNbHOg0USkSUChSLBOPrXj4dd1UATihAp0rz-qODS6T9tY5110uJJcfxLHsCJ14R68woaDLvnUSpYlILFvdcXz6OaOwwJIF4Cv4OvU2DDeC9crPU2NCMhsjCAaRj0f4yKMq35843uZxoa8n6_gXKuhn97qSnnjg1ZwjCNbusNnrNdPuA_3wdB0lSyFmuWKeoUlC2wTTHb4Jc44MeimYGzzlzuXwyZ0uy-O13-nNQabr8ncTnp744nxTUZtp3rwT3xFFeNXPWWRv6rdRcs_QWmdg9S0h2WrifHEx4VlwjFDXXL__VIAJUGDzJHPJtzhS7QmJThnE-g83iSrBjKgWslvVApap5dWksE8VuBjROo8gd2nr5MWQaIr8a2_pFWahz2UHlQTzof8oOT3P0dToHjEcRj3oT3nml2nVf8IM_ttR85kNVPbYhTpeFwhIGwg6ZC2KgQrIjy-duzdLNqOQdFnoIa9HVZaDEDVqvGiSKkJD4gwsU9ckPIBLxA4AIgkFysxyWC2BmM4eHfVQgMsU_iDSsiKwnkpX6TThI07DhHePL8PTlIAex0g8=w597-h929-no?authuser=0",
+        link: "https://song-n-seek.herokuapp.com/",
+        text: "Song N' Seek"
+    },
+    {
+        id:3,
+        img: "https://lh3.googleusercontent.com/RDMKa1GtIj2BU_MkrWkLjLbpE-Kp25tl8knoAST2szAkRhFo6VtoOr6EGXbTRepYjJ8RPt-pDn-08WcSX2in4qptJbS7KYRgnleJSuuFk5gsUWz65Tz-wSH2zW1epsrAxf6lEWhtCi4OYq2E7or0dnYkOUx8INXJB5wJGiicEO-IhTKo-Nv1GsbqFLeSEvntf92ABxn3zoIYASEnLYYQumeJ6PS-vdE6supErCSd4O7qOAaIv-m_Y5_dHUYC3xKPXU4E25dgxmo8oGoG9GJUiz8mQ_F3rosPY_18_3ET-ZLiqnVfVdcZaKvn5LVckl2-hYkiYPgwXAj01B5xZdUpD4CieoNlg4kqhdV3e4YjWzqQeNSk7edEZ2ycurkfzXwi8bF6g9nHQQzLLFkZZfxosAjOf-4IFsfurQhcarttqV0kiQCcCgVezrR9auPIGnODNeJMiwUZUuBQrMk2omcVp8chhJrx8ile6Ho-J2P4HRqmS1fJ-FI31xjXTZF1MPAh_WR7LV0UTnb14bO3jjqQZovhTbJDN6iHlx9Hgu_utbd-ZYiIQTs-LEr6QeUp9KiPbM4lWFidhfgPYlWQy82COJXmFT0nnkHhrNkfySq9XYWIWtHnR1Gg88MUnuJ3lFRanp9Q31CITTNbTjC651JAc-dsCgmKabdaMk0rQIxq7Pk_-YgDB7Q-nBT_O3jLYRXLPMx5T2TBZo4fj29hXNJiGaJ9=w951-h929-no?authuser=0",
+        link: "https://desparta05.github.io/Style-With-My-Weather-App/",
+        text: "Style with my Weather"
+    },
+    {
+        id:4,
+        img: "https://lh3.googleusercontent.com/vhCspUZkeXkexTL1EvocK19jkZkH-pdAr9mr8mjMFL2U9vV3abaqQzdxnkVDptcdZaN-4gwJ3RIwrh_U4IghkqNSv1JIJ3AjHByQatX_uE5ApQgDxNe5eQ0OgVkZQjBr63X4C76wiM6ZuUUCC83IOSJza9IMk-PeBCfqdED6C85FjhMR6v5jQwWckCMLoNhL-xsdm6O-yNEKIOvbuZ_2t0vWLyGoFGaPsZ8CvXFYtCcP68xyy1dlnoPR27izoHo_lNnNYm0_zl8Gzgx4MyTtHQCIFl5WAKH9907m0r8y7mWTK5JU7bi3DnmNFCG54_EParL5su9JGx55AnU_FrOJnZFwhWLI9UXKc-z0F6DSyYoDaACg6FHhKSZz8WmcQ3O-c1bQ4z_X2kYOHtCW5N8gw37qgBYfCSMDc9rw4kRzxF4u2mrxK9-6K2_H1JcIPoadJwgQgq59QP8kOzndNUAZHfwA4KZW952Hd7eiGYNwKuHp70Jf-GTVg701wNGZS0f3WnzgP55zYS-Ff_EgwUG0t9rn0u2yQ_97QUSUXh45CHjk_PUL3WpnjVwBE89fKqckFch048IOtFnP5WMBkBTA2a9F1ludHZ4TwZ710jNCcR-TUUZb_1NlAHDkc-EmhcGIgZ9WoRO0MCzBg0xXTpWgj6Lg01iC-zB4pAH_n8bCHmULho2ZjxN4_lRi_z8hgw0r-ZDwbI2ztKgnKvw4O1o4E8mH=w1480-h929-no?authuser=0",
+        link: "https://desparta05.github.io/BootCampHomework5/index.html",
+        text: "Daily Planner"
+    },
+    {
+        id:5,
+        img: "https://lh3.googleusercontent.com/kjRd9kFno8aoLhnBxj70pb5nD96UDMLgnQzHddg9t2-wd8ePIFMJ8lpspZJtP6q8mJYgWJ6H7jYL_7BD5dj8ms5t3FR4v08JfattA4wUjdiN5zkFth7g2s-QWSoXn99vMyqq4HvyhkwgwVNMtkZQyK1IHh4LmPA1dPXtz3y86acwC9EXd_3SAxNnaswBCoKnBsQ_YXiLpqg4SyK4DS-hRJWkqv9KqVEplmkc-POEGWQrKKATaKzG-PeBZNDodME_rQ2Ta9lV2t2Czn26fNUflGzZ-47mJHRjK4DdsQ_-tqdPfZ3NiHZII7DspfHkL1sjC0Ekxv5EJ2UzrnH_wliP2FcDjiQ1phIp1nlfpeZynEhLws-Oz5UEM4OFRMLBq0czjao592u61LoMtybj4Q9KqKm-QCRcRG5XXMAUx4egz3UfuBcng3nVfuPUcYOcGBWaSXkM4Ls8UeGGM-Bn2EJ9dFU2NQYjQjc1bSXSBFj7LqkAVUTTCQYL2ByhyzlbQYxbO1MpQImyL5Hhdep2TRBfQe9tWCdhWPAgG91PDxvYchedfub_qACxM0uiXW0PtFK32NJr-ZqPmFIHVeVYefEYrKpmF9pIXsNt923r-kNNFWUyWghoFv0pHtzBeSm8ZCe00WDGWTTKLWWHnNTVyt-ayEv8salboFMWTD_vPpX6GcpruHw--OnB-mY_8OGCx2IIth3QPl9AJPnPtNLDoq2nM_Tz=w351-h929-no?authuser=0",
+        link: "https://afternoon-dusk-39802.herokuapp.com/",
+        text: "DC Database"
+    },
+    {
+        id:6,
+        img: "https://lh3.googleusercontent.com/kjRd9kFno8aoLhnBxj70pb5nD96UDMLgnQzHddg9t2-wd8ePIFMJ8lpspZJtP6q8mJYgWJ6H7jYL_7BD5dj8ms5t3FR4v08JfattA4wUjdiN5zkFth7g2s-QWSoXn99vMyqq4HvyhkwgwVNMtkZQyK1IHh4LmPA1dPXtz3y86acwC9EXd_3SAxNnaswBCoKnBsQ_YXiLpqg4SyK4DS-hRJWkqv9KqVEplmkc-POEGWQrKKATaKzG-PeBZNDodME_rQ2Ta9lV2t2Czn26fNUflGzZ-47mJHRjK4DdsQ_-tqdPfZ3NiHZII7DspfHkL1sjC0Ekxv5EJ2UzrnH_wliP2FcDjiQ1phIp1nlfpeZynEhLws-Oz5UEM4OFRMLBq0czjao592u61LoMtybj4Q9KqKm-QCRcRG5XXMAUx4egz3UfuBcng3nVfuPUcYOcGBWaSXkM4Ls8UeGGM-Bn2EJ9dFU2NQYjQjc1bSXSBFj7LqkAVUTTCQYL2ByhyzlbQYxbO1MpQImyL5Hhdep2TRBfQe9tWCdhWPAgG91PDxvYchedfub_qACxM0uiXW0PtFK32NJr-ZqPmFIHVeVYefEYrKpmF9pIXsNt923r-kNNFWUyWghoFv0pHtzBeSm8ZCe00WDGWTTKLWWHnNTVyt-ayEv8salboFMWTD_vPpX6GcpruHw--OnB-mY_8OGCx2IIth3QPl9AJPnPtNLDoq2nM_Tz=w351-h929-no?authuser=0",
+        link: "https://afternoon-dusk-39802.herokuapp.com/",
+        text: "DC Database"
+    },
+]
