cda34a72d7fd7fec270b1e1c18f18839452117a8
Desparta05 <Desparta05@gmail.com>
2022-03-08 23:31:30 -0600

    Added Portfolio & Contact pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Intro from './components/intro/intro';
 import About from './components/about/about';
+import PortfolioList from './components/portfolioList/portfolioList';
+import Contact from './components/contact/contact';
 
 const App = () => (
   <div>
     <p><Intro /></p>
     <p><About /></p>
+    <PortfolioList/>
+    <Contact/>
   </div>
 );
 
