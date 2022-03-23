e5031e8b6923b32f21521084b8f9ad4634658d3d
Desparta05 <Desparta05@gmail.com>
2022-03-23 14:09:32 -0500

    Added Dark Mode Theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Intro from './components/intro/intro';
 import About from './components/about/about';
 import PortfolioList from './components/portfolioList/portfolioList';
 import Contact from './components/contact/contact';
+import Toggle from './components/toggle/toggle';
+import { ThemeContext } from './context';
 
-const App = () => (
-  <div>
-    <p><Intro /></p>
-    <p><About /></p>
+const App = () => {
+
+  const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode;
+  return (
+  <div style={{
+    backgroundColor:darkMode ? "#222" : "white", 
+    color:darkMode && "white"}}>
+    <Toggle/>
+    <Intro/>
+    <About />
     <PortfolioList/>
     <Contact/>
   </div>
-);
+)
+};
 
 export default App;
